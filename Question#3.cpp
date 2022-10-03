332cfea022ca6b4ae3e76d055d4db9db2016e43d
educatiapk <educatiapk@gmail.com>
2022-10-03 04:30:36 -0700

    Question3

--- Question#3.cpp
+++ Question#3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     char type;
@@ -9,17 +8,23 @@ int main()
     cout << "Enter Vahicle Type \n 'L' for Light Vehicle \n 'H' for Heavy Vehicle: ";
     cin >> type;
 
-    cout << "Enter Speed: ";
-    cin >> speed;
-
-    if (type == 'L' && speed <= 120 || type == 'H' && speed <= 110)
+    if (type == 'L' || type == 'H')
     {
-        cout << "Pass... No Fine \n";
+        cout << "Enter Speed: ";
+        cin >> speed;
+
+        if (type == 'L' && speed <= 120 || type == 'H' && speed <= 110)
+        {
+            cout << "Pass... No Fine \n";
+        }
+        else
+        {
+            cout << "Break... Pay Fine \n";
+        }
     }
     else
     {
-        cout << "Break... Pay Fine \n";
+        cout << "Please Enter Correct Vehicle Type \n";
     }
-
     return 0;
 }
